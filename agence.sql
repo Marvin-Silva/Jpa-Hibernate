ba406e971a9ba8b55185637848007fa9ebe8d621
Marvin-Silva <dejesusdasilva.marvin>
2021-03-01 09:05:53 +0100

    ajout BDD

---
+++ agence.sql
@@ -0,0 +1,10 @@
+use `marvin`;
+INSERT INTO `agence`(`adresse`) VALUES
+( 'Nantes'),
+('Brest'),
+('Morlaix'),
+('Pontivy'),
+('Rennes'),
+('Angers'),
+('Laval'),
+('La Roche sur Yon');
---
+++ vehicule.sql
@@ -0,0 +1,10 @@
+use `marvin`;
+INSERT INTO `vehicule`(`geolocalisation`, `marque`,`modele`,`type_energie` ) VALUES
+( 'Nantes', 'Peugeot', '2008 business', 'essence'),
+('Brest', 'Dacia', '2008 business', 'eletrique'),
+('Morlaix', 'Citroen', '2008 business', 'essence'),
+('Pontivy', 'Peugeot', '2008 business', 'eletrique'),
+('Rennes', 'Renault', '2008 business', 'essence'),
+('Angers', 'Bmw', '2008 business', 'eletrique'),
+('Laval', 'Audi', '2008 business', 'essence'),
+('La Roche sur Yon', 'Alfa Romeo', '2008 business', 'eletrique');
